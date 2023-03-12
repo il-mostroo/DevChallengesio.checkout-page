4dd41740f1da7479e7d1964abff27f0acef2754a
il-moostro <elmonstromail@gmail.com>
2023-03-12 15:38:40 +0100

    Add js functionality

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const addItem2=document.querySelector(".plus-sign2")
 const subItem2=document.querySelector(".minus-sign2")
 const itemsNum2=document.querySelector(".quantity2")
 
+const form=document.querySelector('.form')
+const inputFields=document.querySelectorAll('.input-field')
+
 addItem1.addEventListener('click',()=>{
     itemsNum1.textContent=(Number(itemsNum1.textContent)+1).toString()
 })
@@ -23,5 +26,24 @@ subItem1.addEventListener('click',()=>{
 subItem2.addEventListener('click',()=>{
     if(!(itemsNum2.textContent==="0")){
         itemsNum2.textContent=(Number(itemsNum2.textContent)-1).toString()  
+    }
+})
+
+//form validation
+const myCheckbox=document.querySelector(".my-checkbox")
+console.log(myCheckbox)
+form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    let fieldEmpty=1
+    for(let field of inputFields){
+       if(field.value===""){
+        fieldEmpty=0
+       }
+    }
+    if(fieldEmpty){
+        alert('form validated successfully!')
+    }
+    else{
+        alert("Fill up all fileds and try again please!")
     }
 })
