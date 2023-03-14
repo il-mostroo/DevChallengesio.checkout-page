8d42cd942d6a991419e620459eb44cc0d213bf21
il-moostro <elmonstromail@gmail.com>
2023-03-14 01:26:32 +0100

    Refactor code

--- script.js
+++ script.js
@@ -1,92 +1,72 @@
-const addItem1=document.querySelector(".plus-sign1")
-const subItem1=document.querySelector(".minus-sign1")
-const itemsNum1=document.querySelector(".quantity1")
-
-const addItem2=document.querySelector(".plus-sign2")
-const subItem2=document.querySelector(".minus-sign2")
-const itemsNum2=document.querySelector(".quantity2")
-
 const form=document.querySelector('.form')
 const inputFields=document.querySelectorAll('.input-field')
-
-const product1=document.querySelector('.product1')
-const product2=document.querySelector('.product2')
-
-const newPrice1=document.querySelector('.new-price1')
-const newPrice2=document.querySelector('.new-price2')
-
-const signs=document.querySelectorAll('.sign')
-const shipping=document.querySelector('.shipping-price')
-
-const totalPrice=document.querySelector('.total-price')
-addItem1.addEventListener('click',()=>{
-    let price=Number((newPrice1.textContent).slice(1))
-    price+=54.99
-    product1.style.opacity='100%'
-    itemsNum1.textContent=(Number(itemsNum1.textContent)+1).toString()
-    if(Number(itemsNum1.textContent)>0){
-        newPrice1.textContent="$"+price
-    }
+const shippingPriceElement=document.querySelector('.shipping-price')
+const totalPriceElement=document.querySelector('.total-price')
+const productsList=document.querySelectorAll('#products-list>.product')
+let totalPrice=0
+let totalQuantity=2//check this again on load 
+productsList.forEach(product=>{
+    handleProduct(product)
 })
-
-addItem2.addEventListener('click',()=>{
-    let price=Number((newPrice2.textContent).slice(1))
-    price+=54.99
-    product2.style.opacity='100%'
-    itemsNum2.textContent=(Number(itemsNum2.textContent)+1).toString()
-    if(Number(itemsNum2.textContent)>0){
-        newPrice2.textContent="$"+price
-    }
-})
-
-subItem1.addEventListener('click',()=>{
-    let price=Number((newPrice1.textContent).slice(1))
-    price-=54.99
-        if(itemsNum1.textContent==="1"){
-        if(confirm("Do you wanna remove the item from the basket?")){
-            itemsNum1.textContent=(Number(itemsNum1.textContent)-1).toString()  
-            product1.style.opacity='50%'
-            newPrice1.textContent="$0"
+function handleProduct(product){
+    const plusSignElement=product.querySelector(".plus-sign")
+    const minusSignElement=product.querySelector(".minus-sign")
+    const priceElement=product.querySelector('.new-price')
+    const quantityElement=product.querySelector(".quantity")
+    attachAddEventListener(product,plusSignElement,priceElement,quantityElement)
+    attachSubEventListener(product,minusSignElement,priceElement,quantityElement)
+}
+function attachAddEventListener (product,element,priceElement,quantityElement){
+    element.addEventListener('click',()=>{
+        let price=Number(priceElement.dataset.price)
+        const amount=(Number(quantityElement.textContent)+1)
+        quantityElement.textContent=amount.toString()
+        if(Number(quantityElement.textContent)>0){
+            priceElement.textContent="$"+(price*amount).toFixed(2)
         }
-    }
-    else if(!(itemsNum1.textContent=="0")){
-        itemsNum1.textContent=(Number(itemsNum1.textContent)-1).toString()  
-        newPrice1.textContent="$"+price
-    }
-})
-
-subItem2.addEventListener('click',()=>{
-    
-    let price=Number((newPrice2.textContent).slice(1))
-    price-=54.99
-        if(itemsNum2.textContent==="1"){
-        if(confirm("Do you wanna remove the item from the basket?")){
-            itemsNum2.textContent=(Number(itemsNum2.textContent)-1).toString()  
-            product2.style.opacity='50%'
-            newPrice1.textContent="$0"
-        }
-    }
-    else if(!(itemsNum2.textContent=="0")){
-        itemsNum2.textContent=(Number(itemsNum2.textContent)-1).toString()  
-        newPrice2.textContent="$"+price
-    }
-})
-
-signs.forEach(sign=>{
-    sign.addEventListener("click",()=>{
-        const items=Number(itemsNum1.textContent)+Number(itemsNum2.textContent)
-        if(items>=7){
-            shipping.textContent="$38"
-        }
-        else{
-            shipping.textContent="$19"
-        }
-        let totalPriiiice="$"+(Number((newPrice1.textContent).slice(1))+ Number((newPrice2.textContent).slice(1))+Number((shipping.textContent).slice(1))).toString()
-        console.log(totalPriiiice)
-        totalPrice.textContent=totalPriiiice
+        totalPrice+=price*amount
+        totalQuantity++
+        updateShippingCost()
+        updateItemVisibility(amount,product)
     })
 
-})
+}
+
+function attachSubEventListener(product,element,priceElement,quantityElement){
+    element.addEventListener('click',()=>{
+        const price=Number(priceElement.dataset.price)
+        const amount=(Number(quantityElement.textContent)-1)
+        if(quantityElement.textContent==="1"  && confirm("Do you wanna remove the item from the basket?")){
+            quantityElement.textContent=amount.toString()  
+            priceElement.textContent="$0"
+        }
+        else if(quantityElement.textContent!=="0"){
+            quantityElement.textContent=amount.toString()  
+            priceElement.textContent="$"+(price*amount).toFixed(2)
+        }
+        totalPrice-=price*amount
+        totalQuantity--
+        updateShippingCost()
+        updateItemVisibility(amount,product)
+    })
+}
+
+
+
+
+function updateShippingCost(){
+    if(totalQuantity>=7){
+        shippingPriceElement.textContent="$38"
+    }
+    else{
+        shippingPriceElement.textContent="$19"
+    }
+    totalPriceElement.textContent="$"+totalPrice.toFixed(2)
+}
+function updateItemVisibility(amount,product){
+    amount=Math.max(0,amount)
+    product.style.opacity=amount===0? "50%" : "100%"
+}
 
 //form validation
 form.addEventListener('submit',(event)=>{
@@ -105,7 +85,4 @@ form.addEventListener('submit',(event)=>{
     }
 })
 
-//total functionality
 
-
-// alert when user selects 0 quantity
